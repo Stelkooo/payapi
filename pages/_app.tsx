eb6c989aaf96b3bd3201e3ee6b1892cf1f1d17de
stelko <stilyanmtoskov@gmail.com>
2023-08-14 21:58:53 +0100

    fonts and colours

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,27 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { DM_Serif_Display, Public_Sans } from 'next/font/google';
+
+const dmSerifDisplay = DM_Serif_Display({
+  variable: '--font-serif',
+  subsets: ['latin'],
+  weight: '400',
+});
+const publicSans = Public_Sans({ variable: '--font-sans', subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-sans: ${publicSans.style.fontFamily};
+            --font-serif: ${dmSerifDisplay.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-sans)',
+        serif: 'var(--font-serif)',
+      },
+      colors: {
+        'dark-pink': '#BA4270',
+        'link-water-white': '#FBFCFE',
+        'san-juan-blue': '#36536B',
+        'mirage-blue': '#1B262F',
+        'charm-pink': '#DA6D97',
+        'light-san-juan-blue': '#6C8294',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
